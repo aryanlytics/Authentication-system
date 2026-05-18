19b9443127524c470066d2434a9957a528ead4c4
Aryan <aryanlytics@gmail.com>
2026-05-18 16:33:48 +0500

    o

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import App from "./App.tsx";
+import { ClerkProvider } from "@clerk/react";
+
+createRoot(document.getElementById("root")!).render(
+  <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
     <App />
-  </StrictMode>,
-)
+  </ClerkProvider>,
+);
